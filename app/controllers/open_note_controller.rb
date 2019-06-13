275265035ef9a6e224d594da121462de4b5a076c
Ashwin Vidiyala <ashwinvidiyala@gmail.com>
2019-06-13 12:34:57 -0500

    Add OpenNoteController

---
+++ app/controllers/open_note_controller.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+# Creates notes in Bear
+class OpenNoteController < ApplicationController
+  def open_note
+    # user = client.users_info(user: params[:user_id])
+    @slack_client = Slack::Web::Client.new
+
+    @slack_client.chat_postMessage(
+      channel: params[:channel_id],
+      # text: "What is going on <@#{params['user_name']}>?",
+      text: 'Click below to open the note',
+      attachments: [
+        fallback: 'Create your note in Bear?',
+        actions: [
+          {
+            type: 'button',
+            text: 'Open Note',
+            url: "bear://x-callback-url/open-note?title=#{url_encoded_title}",
+            style: 'danger'
+          }
+        ]
+      ]
+    )
+  end
+
+  private
+
+  def url_encoded_title
+    params[:text].gsub(/ +/, '%20')
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  controller :open_note do
+    post 'open_note'
+  end
+
   controller :open_bear do
     post 'open_bear'
   end
