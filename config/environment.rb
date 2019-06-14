6b0bc2c81038db70f20b180ee58f8139bfbad6cd
Ashwin Vidiyala <ashwinvidiyala@gmail.com>
2019-06-14 14:28:26 -0500

    Auto-load classes and directories from the lib directory

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 # Load the Rails application.
 require_relative 'application'
 
+Rails.application.configure do
+  # Custom directories with classes and modules you want to be autoloadable.
+  config.autoload_paths += %W[#{config.root}/lib]
+end
+
 # Initialize the Rails application.
 Rails.application.initialize!
